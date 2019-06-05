29a6635cacfbb93c519fd23af2190e0c172f466d
MrLuss <frederic.reitz@consort-group.com>
2019-06-05 21:36:01 +0200

    Add a simple implementation of isPalindrome

--- src/main/java/fr/consortnt/test/palindrome/checker/impl/PalindromeCheckerServiceImpl.java
+++ src/main/java/fr/consortnt/test/palindrome/checker/impl/PalindromeCheckerServiceImpl.java
@@ -5,7 +5,19 @@ import fr.consortnt.test.palindrome.checker.PalindromeCheckerService;
 public class PalindromeCheckerServiceImpl implements PalindromeCheckerService {
 
 	public boolean isPalindrome(String str) {
-		throw new UnsupportedOperationException("not implemented yet");
+		if(str == null || "".equals(str)) throw new IllegalArgumentException();
+		
+		boolean isPalindrome = true;
+		for (int i = 0; i < (str.length() /2); i++) {
+			int minPostion = i;
+			int maxPosition = str.length() - 1 - i;
+			if (Character.toLowerCase(str.charAt(minPostion)) != Character.toLowerCase(str.charAt(maxPosition))) {
+				isPalindrome = false;
+				break;
+			}
+		}
+		
+		return isPalindrome;
 	}
 
 }
